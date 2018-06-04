3b8df02dae9bf2ca50dcb524a6e5c219ac61d739
Robin Hood <rhcrco@gmail.com>
2018-06-04 01:40:23 -0700

    try again to get to my github

--- HelloWorld/UnitTest1.cs
+++ HelloWorld/UnitTest1.cs
@@ -1,3 +1,16 @@
-﻿<? xml version="1.0" encoding="utf-8" ?>
-<configuration>
-</configuration>
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.AreEqual("Hello World!", Program.CreateMessage());
+        }
+    }
+}
