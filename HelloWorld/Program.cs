a893d12aa89ef6551afd70ad331d0ecc7a8a3c9e
Robin Hood <rhcrco@gmail.com>
2018-06-03 20:34:22 -0700

    First commit working toward jenkins implementation

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+        public static string CreateMessage() 
+        {
+            return "Hello World";
+        }
+    }
+}
