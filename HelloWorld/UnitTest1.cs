69f862da70acdc180749e68f2b43263465df69ab
robinhood-me <rhcrco@gmail.com>
2018-06-09 21:21:56 -0700

    Am trying Nunit instead of MSTest to see if I can get testing working on a MAC.

--- HelloWorld/UnitTest1.cs
+++ HelloWorld/UnitTest1.cs
@@ -1,5 +1,18 @@
 ﻿using System;
+#if NUNIT
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+#else
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+using NUnitAssert = NUnit.Framework.Assert;
+using MsAssert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
 using HelloWorld;
 
 // Why won't it push my changes to my github repo?
